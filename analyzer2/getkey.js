ea4e4ea8d4008df76bc5403c0710e794766c619f
KHashimoto3 <hashi.koki385@gmail.com>
2024-06-08 16:52:12 +0900

    入力された文字や文字数、削除された文字や文字数をコンソールに出力できるようにした。

--- analyzer2/getkey.js
+++ analyzer2/getkey.js
@@ -4,19 +4,61 @@ const fs = require("fs");
 
 const fileName = "A01_sample_video.json";
 
-const printValue = (value) => {
+const printValue = (values) => {
   console.log("読み込んだファイルの内容: ");
-  value.map((v) => {
-    console.log(JSON.stringify(v, null, "  "));
+  //最後のvalueは分析に使わないので削除
+  values.pop();
+  values.map((value) => {
+    const keyData = getkeyData(value);
+    const inputText = keyData.input.map((t) => `'${t}'`).join(", ");
+    const removedText = keyData.removed.map((t) => `'${t}'`).join(", ");
+    console.log(
+      ` ${keyData.timestamp}\t\t${keyData.inputSize}\t${inputText}\t\t\t${keyData.removedSize}\t${removedText}`
+    );
   });
+};
+
+//valueの中身を取り出す
+const getkeyData = (valueObj) => {
+  const timestamp = valueObj.timestamp;
+  const input = valueObj.changeData.text;
+  const inputSize = countInputSize(input);
+  const removed = valueObj.changeData.removed;
+  const removedSize = countRemovedSize(removed);
+  const keyData = {
+    timestamp: timestamp,
+    input: input,
+    inputSize: inputSize,
+    removed: removed,
+    removedSize: removedSize,
+  };
+  return keyData;
+};
+
+//入力文字数のカウント
+const countInputSize = (input) => {
+  let size = 0;
+  input.map((t) => {
+    size += t.length;
+  });
+  return size;
+};
+
+//削除文字数のカウント
+const countRemovedSize = (removed) => {
+  let size = 0;
+  removed.map((t) => {
+    size += t.length;
+  });
+  return size;
 };
 
 //ファイルを読み込む（同期）
 try {
   const jsonString = fs.readFileSync("record-files/" + fileName, "utf8");
   const obj = JSON.parse(jsonString);
-  printValue(obj.value);
   console.log("読み込んだファイルの内容: ", obj.header);
+  printValue(obj.value);
 } catch (error) {
   console.log("エラーが発生しました: ", error);
 }
