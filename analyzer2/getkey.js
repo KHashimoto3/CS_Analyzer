fed84ad0e9ffcdc162f35eada9a326b13df42930
KHashimoto3 <hashi.koki385@gmail.com>
2024-06-08 15:42:54 +0900

    jsonファイルを読み込んでデータをオブジェクトごと表示できるようにした。

--- analyzer2/getkey.js
+++ analyzer2/getkey.js
@@ -1,1 +1,22 @@
 console.log("hello from getkey.js");
+
+const fs = require("fs");
+
+const fileName = "A01_sample_video.json";
+
+const printValue = (value) => {
+  console.log("読み込んだファイルの内容: ");
+  value.map((v) => {
+    console.log(JSON.stringify(v, null, "  "));
+  });
+};
+
+//ファイルを読み込む（同期）
+try {
+  const jsonString = fs.readFileSync("record-files/" + fileName, "utf8");
+  const obj = JSON.parse(jsonString);
+  printValue(obj.value);
+  console.log("読み込んだファイルの内容: ", obj.header);
+} catch (error) {
+  console.log("エラーが発生しました: ", error);
+}
