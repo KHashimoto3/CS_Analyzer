a183db2d0caa26d97b67a873756c801819f7dcbe
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-03 18:04:09 +0900

    打鍵の一覧データもJSONで記録するようにした。

--- analyzer2/get-key.js
+++ analyzer2/get-key.js
@@ -17,6 +17,7 @@ const printKeyDatas = (keyDatas, startTime) => {
   let removedCout = 0; //削除文字数
   let inputDataCount = 0; //入力データ数
   let removedDataCount = 0; //削除データ数
+  let adjustedKeyDatas = [];
   keyDatas.map((keyData) => {
     if (keyData.inputSize > 0) {
       inputCount = inputCount + keyData.inputSize;
@@ -27,13 +28,16 @@ const printKeyDatas = (keyDatas, startTime) => {
       removedDataCount++;
     }
     const timestamp = keyData.timestamp - startTime;
-    //inputおよびremovedのそれぞれの文字を''で囲み、カンマ区切りに整形
-    const inputText = keyData.input.map((t) => `'${t}'`).join(", ");
-    const removedText = keyData.removed.map((t) => `'${t}'`).join(", ");
-    //以下のコメントアウトを外すと、詳細な打鍵記録を表示
-    /*console.log(
-      ` ${timestamp}\t\t${keyData.inputSize}\t${inputText}\t\t\t${keyData.removedSize}\t${removedText}`
-    );*/
+
+    const adjustedKeyData = {
+      timestamp: timestamp,
+      inputSize: keyData.inputSize,
+      input: keyData.input,
+      removedSize: keyData.removedSize,
+      removed: keyData.removed,
+    };
+    adjustedKeyDatas.push(adjustedKeyData);
+
     endTime = timestamp;
   });
   const typePerSec = valueCount / (endTime / 1000);
@@ -56,6 +60,7 @@ const printKeyDatas = (keyDatas, startTime) => {
     removedDataCount: removedDataCount,
     totalTime: endTime, //ms
     typePerSec: typePerSec, //個/秒
+    adjustedKeyDatas: adjustedKeyDatas,
   };
   return result;
 };
@@ -108,9 +113,10 @@ try {
   const obj = JSON.parse(jsonString);
   console.log("読み込んだファイルのヘッダー: ", obj.header);
   const values = obj.value;
+  const startTime = obj.header.timeStampStart;
 
   const keyDatas = getkeyDatas(values);
-  const result = printKeyDatas(keyDatas, 0);
+  const result = printKeyDatas(keyDatas, startTime);
 
   console.log("分析完了しました！！！！");
   console.log("以下をファイルに書き込みました。 ");
