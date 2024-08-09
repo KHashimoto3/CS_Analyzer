709703753c3bef9e61444a7b5387da76227ec40d
KHashimoto3 <hashi.koki385@gmail.com>
2024-08-09 10:59:15 +0900

    重要なデータを表で確認できるようにした。

--- analyze-viewer/src/App.tsx
+++ analyze-viewer/src/App.tsx
@@ -1,3 +1,12 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import data from "./result-merged.json";
 
 function App() {
@@ -5,12 +14,53 @@ function App() {
     <>
       <h1>Analyze-viewer</h1>
       <p>分析結果のビューア</p>
-      <textarea
-        rows={30}
-        cols={100}
-        value={JSON.stringify(data, null, 2)}
-        readOnly
-      ></textarea>
+      <p>分析のリスト</p>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>name</TableCell>
+              <TableCell>title</TableCell>
+              <TableCell>recNumber</TableCell>
+              <TableCell>datasCount</TableCell>
+              <TableCell>inputDataCount</TableCell>
+              <TableCell>removedDataCount</TableCell>
+              <TableCell>missTypeRate</TableCell>
+              <TableCell>totalTime</TableCell>
+              <TableCell>typePerSec</TableCell>
+              <TableCell>totalReInputCnt</TableCell>
+              <TableCell>totalReInputTime</TableCell>
+              <TableCell>reInputRate</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row: any) => (
+              <TableRow key={row.header.name}>
+                <TableCell component="th" scope="row">
+                  {row.header.name}
+                </TableCell>
+                <TableCell>{row.header.title}</TableCell>
+                <TableCell>{row.header.recNumber}</TableCell>
+                <TableCell>{row.result.datasCount}</TableCell>
+                <TableCell>{row.result.inputDataCount}</TableCell>
+                <TableCell>{row.result.removedDataCount}</TableCell>
+                <TableCell>
+                  {parseFloat(row.result.missTypeRate.toFixed(3))}
+                </TableCell>
+                <TableCell>{row.result.totalTime}</TableCell>
+                <TableCell>
+                  {parseFloat(row.result.typePerSec.toFixed(3))}
+                </TableCell>
+                <TableCell>{row.result.totalReInputCnt}</TableCell>
+                <TableCell>{row.result.totalReInputTime}</TableCell>
+                <TableCell>
+                  {parseFloat(row.result.reInputRate.toFixed(3))}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
