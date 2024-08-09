32ebc52a3ffc81e3f57c0a45c4a97ce659eb2a7a
KHashimoto3 <hashi.koki385@gmail.com>
2024-08-09 10:31:30 +0900

    記録されたJSONデータを仮で表示できるようにした。

--- analyze-viewer/src/App.tsx
+++ analyze-viewer/src/App.tsx
@@ -1,8 +1,16 @@
+import data from "./result-merged.json";
+
 function App() {
   return (
     <>
       <h1>Analyze-viewer</h1>
       <p>分析結果のビューア</p>
+      <textarea
+        rows={30}
+        cols={100}
+        value={JSON.stringify(data, null, 2)}
+        readOnly
+      ></textarea>
     </>
   );
 }
