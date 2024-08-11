3b21d9824c3fc4d01c7a5107665f750dd8141268
KHashimoto3 <hashi.koki385@gmail.com>
2024-08-11 10:20:34 +0900

    平均したものを１つのファイルに集計するプログラムを作成した。

---
+++ analyzer2/merge-average-result.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+
+// JSONファイルが保存されているディレクトリを指定
+const jsonDirectory = path.join(__dirname, "average-result-files");
+
+// 出力ファイルのパスを指定
+const outputFile = path.join(__dirname, "average-result-merged.json");
+
+// ディレクトリ内のすべてのJSONファイルを読み込んで結合
+fs.readdir(jsonDirectory, (err, files) => {
+  if (err) {
+    console.error("ディレクトリをスキャンできません: " + err);
+    return;
+  }
+
+  const results = [];
+
+  files.forEach((file) => {
+    if (file.endsWith(".json")) {
+      const filePath = path.join(jsonDirectory, file);
+      const fileContent = fs.readFileSync(filePath, "utf8");
+      const jsonData = JSON.parse(fileContent);
+
+      const outputObj = {
+        header: jsonData.header,
+        result: jsonData.resultAverage,
+      };
+
+      results.push(outputObj);
+    }
+  });
+
+  // 配列のまま書き出す
+  fs.writeFileSync(outputFile, JSON.stringify(results, null, 2), "utf8");
+  console.log("結合されたJSONが", outputFile, "に書き出されました");
+});
